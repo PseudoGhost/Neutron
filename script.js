139aabd078d4a4046091e2cd2f5e74ff06c89c66
PseudoGhost <151562727+PseudoGhost@users.noreply.github.com>
2024-02-24 14:28:17 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const CORRECT_PASSWORD = "neutron12";
 
+function checkPassword() {
+    const passwordInput = document.getElementById("passwordInput").value;
+    if (passwordInput === CORRECT_PASSWORD) {
+        openCustomPage();
+    } else {
+        document.getElementById("errorMessage").style.display = "block";
+    }
+}
+
+function openCustomPage() {
+    const newWindow = window.open("home.html", "_blank");
+}
