435f5f0e58297b1637d1631a94a416e088c179a2
PseudoGhost <151562727+PseudoGhost@users.noreply.github.com>
2024-02-24 14:31:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,18 @@ function checkPassword() {
 }
 
 function openCustomPage() {
+    // Open a new window with about:blank
     const newWindow = window.open("about:blank", "_blank");
     
+    // Once the window is loaded, set its content to home.html
     newWindow.addEventListener("load", function() {
         fetch("home.html")
             .then(response => response.text())
-            .then(html => newWindow.document.write(html))
+            .then(html => {
+                newWindow.document.open();
+                newWindow.document.write(html);
+                newWindow.document.close();
+            })
             .catch(error => console.error("Error loading content:", error));
     });
 }
