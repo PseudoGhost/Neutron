cf818865b6cbc4f24ddff57b12ff4bebeb969d69
PseudoGhost <151562727+PseudoGhost@users.noreply.github.com>
2024-02-26 11:36:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function checkPassword() {
 function openCustomPage() {
     const newWindow = window.open("home.html", "_blank");
 }
+
+function changeGame(game) {
+    let title = document.getElementById('title');
+    let gameDict = {'Diablo JS': 'DiabloJS/'};
+    let gameIframe = document.getElementById('gameIframe');
+    let introText = document.getElementById('introText');
+    let fullscreenButton = document.getElementById('fullscreenButton');
+    
+    fullscreenButton.style.display = 'block';
+    title.innerHTML = game;
+    introText.style.display = 'none';
+    gameIframe.src = gameDict[game];
+    gameIframe.style.display = 'block';
+    
+    let items = document.querySelectorAll('a.active');
+    for (let elem of items) {
+        elem.classList.remove('active');
+    };
+    let element = document.getElementById(game);
+    element.classList.add('active');
+}
